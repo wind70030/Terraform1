1f6c255677a5a6fdfd73f388b34ca5ca44a4ca88
wind70030 <114542252+wind70030@users.noreply.github.com>
2023-10-10 11:12:09 +0900

    Update securitygroups-eks.tf

--- securitygroups-eks.tf
+++ securitygroups-eks.tf
@@ -1,4 +1,93 @@
-# AWS EC2 Security Group Terraform Module
+
+## 퍼블릭 보안 그룹
+resource "aws_security_group" "Public-SG" {
+  vpc_id = module.vpc.vpc_id
+  name = "public SG"
+  description = "public SG"
+  tags = {
+    Name = "public SG"
+  }
+}
+## 프라이빗 보안 그룹
+resource "aws_security_group" "Private-SG" {
+  vpc_id = module.vpc.vpc_id
+  name = "private SG"
+  description = "private SG"
+  tags = {
+    Name = "private SG"
+  }
+}
+## 퍼블릭 보안 그룹 규칙
+resource "aws_security_group_rule" "PublicSGRulesHTTPingress" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.Public-SG.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group_rule" "PublicSGRulesHTTPegress" {
+  type = "egress"
+  from_port = 80
+  to_port = 80
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.Public-SG.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group_rule" "PublicSGRulesHTTPSingress" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.Public-SG.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group_rule" "PublicSGRulesHTTPSegress" {
+  type = "egress"
+  from_port = 443
+  to_port = 443
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.Public-SG.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "PrivateSGRulesRDSingress" {
+  type = "ingress"
+  from_port = 3306
+  to_port = 3306
+  protocol = "TCP"
+  security_group_id = aws_security_group.Private-SG.id
+  source_security_group_id = aws_security_group.Public-SG.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group_rule" "PrivateSGRulesRDSegress" {
+  type = "egress"
+  from_port = 3306
+  to_port = 3306
+  protocol = "TCP"
+  security_group_id = aws_security_group.Private-SG.id
+  source_security_group_id = aws_security_group.Public-SG.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+/* # AWS EC2 Security Group Terraform Module
 # Security Group for Public Bastion Host
 module "eks_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -13,4 +102,4 @@ module "eks_sg" {
   # Egress Rule - all-all open
   egress_rules = ["all-all"]
   tags = local.common_tags
-}
+} */
