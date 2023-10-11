58fca6162b53ac0276c7e13a781003c14e3423e2
wind70030 <114542252+wind70030@users.noreply.github.com>
2023-10-11 15:33:52 +0900

    Update rds.tf

--- test-terraform/rds.tf
+++ test-terraform/rds.tf
@@ -1,15 +1,12 @@
 #rds의 서브넷 그룹으로 사용할 subnet들 미리 지정
 
-/* resource "aws_db_subnet_group" "realDBSubnetGroup" {
-  name = "test"
-    subnet_ids = [
-    aws_subnet.module.vpc.database_subnets,
-  ]
+resource "aws_db_subnet_group" "realDBSubnetGroup" {
+  name = "db_subnet_group"
   subnet_ids = module.vpc.database_subnets
   tags = {
-    "Name" = "real-db-subnet-group"
+    "Name" = "db-subnet-group"
   }
-} */
+}
 
 resource "aws_db_instance" "database" { 
     allocated_storage = 50
@@ -18,14 +15,15 @@ resource "aws_db_instance" "database" {
 #보안그룹 지정.
     vpc_security_group_ids = [aws_security_group.DB-SG.id ]
 #서브넷 그룹 지정.
-    db_subnet_group_id = module.vpc.database_subnets
+    db_subnet_group_name = "${aws_db_subnet_group.realDBSubnetGroup.id}"
     publicly_accessible = false
     engine = "mariadb"
     engine_version = "10.6.8"
     instance_class = "db.t3.small"
-    multi_az = true
-    availability_zone = "ap-northeast-2a"
-    /* db_name = "testDB" */
+    /* multi_az = true
+    availability_zone = "ap-northeast-2a" */
+    identifier = "test-rdb"
+    name = "testRDB"
     username = "admin"
     password = "testtest"
     tags = {
