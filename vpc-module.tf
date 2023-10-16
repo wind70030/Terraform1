aed2697726bf2f857be1e53e3d3858d806632092
wind70030 <114542252+wind70030@users.noreply.github.com>
2023-10-16 16:56:15 +0900

    Update vpc-module.tf

--- vpc-module.tf
+++ vpc-module.tf
@@ -16,11 +16,11 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets  
 
   # Database Subnets
-  #database_subnets = var.vpc_database_subnets
-  #create_database_subnet_group = var.vpc_create_database_subnet_group
-  #create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
-  # create_database_internet_gateway_route = true
-  # create_database_nat_gateway_route = true
+  database_subnets = var.vpc_database_subnets
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  create_database_internet_gateway_route = false
+  create_database_nat_gateway_route = true
   
   # NAT Gateways - Outbound Communication
   enable_nat_gateway = var.vpc_enable_nat_gateway 
@@ -47,7 +47,7 @@ module "vpc" {
     "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"    
   }
 
-  /* database_subnet_tags = {
+  database_subnet_tags = {
     Type = "database-subnets"
-  } */
+  }
 }
