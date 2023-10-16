c2147b00a7c9b5e736de20fd6e83fc13d45f7bfc
wind70030 <114542252+wind70030@users.noreply.github.com>
2023-10-16 16:55:26 +0900

    Update vpc-variables.tf

--- vpc-variables.tf
+++ vpc-variables.tf
@@ -26,39 +26,38 @@ variable "vpc_availability_zones" {
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
-  default = ["10.10.101.0/24", "10.10.102.0/24"]
+  default = ["10.10.1.0/24", "10.10.2.0/24"]
 }
 
 # VPC Private Subnets
 variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
-  default = ["10.10.1.0/24", "10.10.2.0/24"]
+  default = ["10.10.100.0/24", "10.10.101.0/24"]
 }
 
-/*
+
 # VPC Database Subnets
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
-  default = ["10.0.151.0/24", "10.0.152.0/24"]
+  default = ["10.10.200.0/24", "10.10.201.0/24"]
 }
-*/
+
 # VPC Create Database Subnet Group (True / False)
-/* variable "vpc_create_database_subnet_group" {
+variable "vpc_create_database_subnet_group" {
   description = "VPC Create Database Subnet Group"
   type = bool
   default = true 
-} */
+}
 
-/*
 # VPC Create Database Subnet Route Table (True or False)
 variable "vpc_create_database_subnet_route_table" {
   description = "VPC Create Database Subnet Route Table"
   type = bool
   default = true   
 }
-*/
+
   
 # VPC Enable NAT Gateway (True or False) 
 variable "vpc_enable_nat_gateway" {
